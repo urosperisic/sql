be86730b2970751ca0b51c0e484a86f8c0fd546d
urosperisic <uros2907@gmail.com>
2025-05-10 22:27:24 +0200

    004 added 003_progdiary folder, added create_table(), add_entry(), and get_entries().

---
+++ 003_progdiary/app.py
@@ -0,0 +1,32 @@
+from database import create_table, add_entry, get_entries
+
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View all entries.
+3) Exit.
+
+Your selection: """
+
+welcome = "Welcome to the programming diary!"
+
+def prompt_new_entry():
+    entry_content = input("What have you learned today? : ")
+    entry_date = input("Enter the date: ")
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+print(welcome)
+
+create_table()
+
+while (user_input := input(menu)) != "3":
+    if user_input == "1":
+        prompt_new_entry()
+    elif user_input == "2":
+        view_entries(get_entries())
+    else:
+        print("Invalid option, please try again...")
+
---
+++ 003_progdiary/database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+
+connection.row_factory = sqlite3.Row
+
+def create_table():
+    # connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
+    # connection.commit()
+    with connection:
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
+
+def add_entry(entry_content, entry_date):
+    with connection:
+        connection.execute("INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date))
+
+def get_entries():
+   cursor = connection.execute("SELECT * FROM entries;")
+   return cursor
